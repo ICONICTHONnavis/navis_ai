dc2ea13fb32627e7b6711fd5ea13cab845de85cd
teom142 <xodn9245@naver.com>
2024-11-15 04:50:05 +0900

    feat: add recommend subject

--- config/subject/utils.py
+++ config/subject/utils.py
@@ -1,18 +1,16 @@
 from .models import complete_tb, subject_tb
-from .serializers import recommendSerializer
-from user.models import user_tb
-
 from langchain.chat_models import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
-from .template import gen_nl_template
+from .template import gen_nl_template, recommend_template
 from langchain_core.messages import SystemMessage
-from langchain_core.output_parsers import StrOutputParser
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
 def gen_desc(user_id):
     complete_subject = complete_tb.objects.filter(user_id=user_id).values()
     data = []
+    if complete_subject == None:
+        return None
     for i in complete_subject:
         subject_name = subject_tb.objects.get(id=i['subject_id']).name
         data.append(
@@ -23,10 +21,11 @@ def gen_desc(user_id):
             }
         )
         # data = i["grade"] + " | " + subject_name + " | " + i["semester"]
-    llm = ChatOpenAI(temperature=0.0,  # 창의성 (0.0 ~ 2.0)
-                     max_tokens=2048,  # 최대 토큰수
-                     model_name='gpt-4o',  # 모델명
-                     )
+    llm = ChatOpenAI(
+        temperature=0.0,  # 창의성 (0.0 ~ 2.0)
+        max_tokens=2048,  # 최대 토큰수
+        model_name='gpt-4o',  # 모델명
+    )
     memory = ConversationBufferMemory()
     system_message = SystemMessage(content=gen_nl_template)
     human_message = HumanMessagePromptTemplate.from_template("current content: {history}, <data>: {input}")
@@ -37,3 +36,22 @@ def gen_desc(user_id):
         memory=memory,
     )
     return conversation.invoke(str(data))["response"]
+
+def subject_recommend(user_desc):
+    llm = ChatOpenAI(
+        temperature=0.0,  # 창의성 (0.0 ~ 2.0)
+        max_tokens=2048,  # 최대 토큰수
+        model_name='gpt-4o',  # 모델명
+    )
+    memory = ConversationBufferMemory()
+    system_message = SystemMessage(content=recommend_template)
+    human_message = HumanMessagePromptTemplate.from_template("current content: {history}, <data>: {input},")
+    user_prompt = ChatPromptTemplate(messages=[system_message, human_message])
+    conversation = ConversationChain(
+        prompt=user_prompt,
+        llm=llm,
+        memory=memory,
+    )
+    return conversation.invoke({
+        "input": user_desc,
+    })["response"]
--- config/subject/views.py
+++ config/subject/views.py
@@ -5,7 +5,8 @@ from rest_framework.response import Response
 from .models import complete_tb, subject_tb
 from .serializers import recommendSerializer
 from user.models import user_tb
-from .utils import gen_desc
+from chat.serializers import chatSerializer
+from .utils import gen_desc, subject_recommend
 
 @api_view(['POST'])
 def recommend_subject(request):
@@ -14,9 +15,20 @@ def recommend_subject(request):
         studentNumber = serializer.data['studentNumber']
         try:
             user = user_tb.objects.get(student_number=studentNumber)
-            desc = gen_desc(user.id)
+            # desc = gen_desc(user.id)
+            rec = subject_recommend(user.nl_description)
+            data = {
+                "answer": rec,
+                "question": "나의 학점 정보를 기반으로 들을만한 전공 수업 추천해줘",
+                "user": user.id
+            }
+            chat_serializer = chatSerializer(data=data)
+            chat_serializer.is_valid(raise_exception=True)
+            chat_serializer.save()
             return Response({
-                "responseDto": desc,
+                "responseDto": {
+                    "chattingAnswer": rec,
+                },
                 "error": None,
                 "success": True
             }, status=status.HTTP_200_OK)
