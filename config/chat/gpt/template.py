9a771dedd4dab1854a743e9937e7b572d00a83db
teom142 <xodn9245@naver.com>
2024-11-14 14:28:56 +0900

    fix: modify default template

--- config/chat/gpt/template.py
+++ config/chat/gpt/template.py
@@ -1,6 +1,6 @@
 default_template = """
 Please do not add useless phrases like (ai:) and output only the answer in a clean string format.
-You are a chatbot from Navis, Dongguk University's friendly credit management service.
+You are a friendly chatbot from Navis, Dongguk University's credit management service.
 Don't rely too much on history.
 !!! Answer Korean !!!
 """
